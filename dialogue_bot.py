5906d2fd465d4e8cbf2f8732d7a3d0101eff58eb
myngshj3 <myngshj3@gmail.com>
2023-09-12 10:31:30 +0900

    大規模テキストデータを用いてトレーニングするメソッド追加。

--- dialogue_bot.py
+++ dialogue_bot.py
@@ -1,7 +1,8 @@
 import json
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, LineByLineTextDataset
 from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import AutoModelForMaskedLM, AutoConfig, DataCollatorForLanguageModeling, Trainer, TrainingArguments
 
 
 class DialogueBot:
@@ -88,3 +89,40 @@ class DialogueBot:
             input_text: str = input("Input question:")
             response: str = self.have_dialogue(input_text)
             print("Bot:", response)
+
+    def train_by_bigtext(self, file_path):
+        # データセットの読み込みと前処理
+        # テキストファイルからデータを読み込む
+        dataset = LineByLineTextDataset(
+            tokenizer=self.tokenizer,
+            file_path=file_path,  # テキストファイルのパス
+            block_size=self.config["dataset_block_size"],  # ブロックサイズ（トークン数）
+        )
+        # データコレーターの設定
+        data_collator = DataCollatorForLanguageModeling(
+            tokenizer=self.tokenizer, mlm=False
+        )
+
+        # トレーニングアーギュメントの設定
+        training_args = TrainingArguments(
+            output_dir=self.config["training_output_dir"],
+            overwrite_output_dir=self.config["overwrite_output_dir"],
+            num_train_epochs=self.config["num_train_epochs"],
+            per_device_train_batch_size=self.config["per_device_train_batch_size"],
+            save_steps=self.config["training_save_steps"],
+            save_total_limit=self.config["training_save_total_limit"],
+        )
+
+        # トレーナーの設定
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            data_collator=data_collator,
+            train_dataset=dataset,
+        )
+
+        # ファインチューニングの実行
+        trainer.train()
+
+        # ファインチューニングされたモデルの保存
+        trainer.save_model()
